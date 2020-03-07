02dc76d36af2518269d1fc5eaeaf84a83543afdf
Paulo Tupiná <pftupina@gmail.com>
2020-03-07 11:28:28 -0300

    Gameplay basico

---
+++ Assets/Scripts/GameButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ScriptableObjectArchitecture;
+using System;
+
+public class GameButton : MonoBehaviour
+{
+    [SerializeField]
+    Button button;
+
+    [SerializeField]
+    Color mainColor;
+
+
+    [SerializeField]
+    Color highlightColor;
+
+    private void Start()
+    {
+        button.image.color = mainColor;   
+    }
+
+
+    public void glow()
+    {
+        StartCoroutine(color(button));
+    }
+
+    IEnumerator color(Button button)
+    {
+
+        button.image.color = mainColor;
+        yield return new WaitForSeconds(0.3f);
+        button.image.color = highlightColor;
+        yield return new WaitForSeconds(0.3f);
+        button.image.color = mainColor;
+
+    }
+}
+
+
---
+++ Assets/Scripts/PseudoGameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PseudoGameManager : MonoBehaviour
+{
+    [SerializeField]
+    GameButton[] gameButtons;
+
+
+    [SerializeField]
+    List<int> buttonOrder;
+
+    private int expectedButtonIndex;
+
+    private void Start()
+    {
+        buttonOrder = new List<int>();
+
+        addButtonToOrder();
+        StartCoroutine(showOrder());
+
+    }
+
+    void addButtonToOrder()
+    {
+        int n = Random.Range(0, gameButtons.Length);
+        buttonOrder.Add(n);
+    }
+
+    IEnumerator showOrder()
+    {
+
+        yield return new WaitForSeconds(1f);
+        foreach (int n in buttonOrder)
+        {
+            gameButtons[n].glow();
+            yield return new WaitForSeconds(1f);
+        }
+        expectedButtonIndex = 0;
+    }
+
+    public void buttonPressed(int n)
+    {
+        if (n == buttonOrder[expectedButtonIndex])
+        {
+            expectedButtonIndex++;
+
+            if (expectedButtonIndex >= buttonOrder.Count)
+            {
+                addButtonToOrder();
+                StartCoroutine(showOrder());
+
+            }
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+}
