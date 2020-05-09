2de2f8e8e56e134c80dfaa2360f61def9e47b2fb
Paulo Felipe Tupiná <pftupina@gmail.com>
2020-05-09 15:12:49 -0300

    Mudando lógica de animação
    Closes https://github.com/pftupina/GeniusUnity/issues/3

--- Assets/Scripts/GameButton.cs
+++ Assets/Scripts/GameButton.cs
@@ -13,12 +13,6 @@ public class GameButton : MonoBehaviour
     [SerializeField]
     int buttonId;
 
-    [SerializeField]
-    Color mainColor;
-
-    [SerializeField]
-    Color highlightColor;
-
     [Header("Events")]
     [SerializeField]
     IntGameEvent buttonPressedEvent;
@@ -26,16 +20,9 @@ public class GameButton : MonoBehaviour
     [SerializeField]
     UnityEvent onGlow;
 
-
     [Header("Component references")]
     [SerializeField]
     Button buttonComponent;
-
-
-    private void OnValidate()
-    {
-        buttonComponent.image.color = mainColor;
-    }
 
     public void buttonPressed()
     {
@@ -48,22 +35,6 @@ public class GameButton : MonoBehaviour
         {
             onGlow.Invoke();
         }
-    }
-
-    public void glow()
-    {
-        _ = StartCoroutine(color(buttonComponent));
-    }
-
-    IEnumerator color(Button button)
-    {
-
-        button.image.color = mainColor;
-        yield return new WaitForSeconds(0.3f);
-        button.image.color = highlightColor;
-        yield return new WaitForSeconds(0.3f);
-        button.image.color = mainColor;
-
     }
 }
 
